2b2024c903e8ff8a0b3c9ab3fafec5796bb39f60
hannahmbee <31848924+hannahmbee@users.noreply.github.com>
2017-11-01 18:11:17 +0000

    second changes

--- js/main.js
+++ js/main.js
@@ -8,24 +8,20 @@ $(document).ready(function() {
 		autoplaySpeed: 3000
 	});
 
-//Might change this to have the preview ontop of the image
-
+// INDEX.HTML: Might change this to have the preview ontop of the image with hover
+// Somehow use 'this'... 
 /* User clicks image
 	<p> slides down
 	"Read Less >" is shown */
-	$('.read-more').click(function(e) {
-		e.preventDefault();
-		$('#show-on-click').slideDown();
-		$('.read-less').show();
-	})
 
 /* User clicks "Read Less >" 
-	<p> slides up */
-	$('.read-less').click(function(e) {
-		e.preventDefault();
-		$('#show-on-click').slideUp();
-		$('.read-less').hide();
-	})
+	<p> slides up 
+	"Read Less >" is hidden */
 
+// Make a dropdown menu
+	/* When user clicks "Series"
+		List drops down
+		Links to each series 'homepage'
+		Disappears when user clicks out of menu*/
 
 });
